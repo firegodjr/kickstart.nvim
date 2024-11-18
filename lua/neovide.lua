c00607165c8fd9ba7bead45b2cee00231fc7e0c5
Ethan Schrunk <ethan.schrunk@knowledgelake.com>
2024-11-18 12:28:53 -0600

    Additional neovide config for work vm

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -7,3 +7,20 @@ vim.o.guifont = "CaskaydiaCove Nerd Font:h11"
 -- Disable shade b/c it blurs everything
 local shade = require('shade')
 shade.toggle()
+
+-- Animate when changing mode
+vim.g.neovide_cursor_vfx_mode = "wireframe"
+
+-- Specific things for work VM
+if vim.fn.hostname() == "2017EXCLUSIONZO" then
+  vim.cmd([[
+    augroup cdpwd
+      autocmd!
+      autocmd VimEnter * cd C:/dev
+    augroup END
+  ]])
+  vim.o.guifont = "CaskaydiaCove Nerd Font:h10"
+  vim.g.neovide_refresh_rate = 30
+  vim.g.neovide_floating_blur_amount_x = 0
+  vim.g.neovide_floating_blur_amount_y = 0
+end
