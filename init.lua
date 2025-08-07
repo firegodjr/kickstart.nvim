a8d58d78bc070263622dd9f167526ea288f67c98
Ethan Schrunk <ethan.schrunk@knowledgelake.com>
2025-08-07 09:42:58 -0500

    Update packages, disable TS for large files

--- init.lua
+++ init.lua
@@ -772,6 +772,7 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    lazy = false,
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
@@ -794,6 +795,14 @@ require('lazy').setup({
       auto_install = true,
       highlight = {
         enable = true,
+        -- Disable for large files
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
