08b5449b3d66eb84dfdbb0c590f931c26d4f3b48
firegodjr <firegodjr.es@gmail.com>
2025-08-11 11:05:52 -0500

    Fix plugin order

--- lua/core.lua
+++ lua/core.lua
@@ -16,7 +16,59 @@ Plugins in this file:
 
 -- Consolidate all plugins into lua/plugins
 return {
-  { import = 'plugins' },
+  { -- Show pending keybinds
+    'folke/which-key.nvim',
+    event = 'VimEnter',
+    config = function(self, opts)
+      local wk = require 'which-key'
+      wk.add {
+        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>g', group = '[G]it' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+      }
+    end,
+    opts = {
+      icons = {
+        mappings = vim.g.have_nerd_font,
+        keys = vim.g.have_nerd_font and {} or {
+          Up = '<Up> ',
+          Down = '<Down> ',
+          Left = '<Left> ',
+          Right = '<Right> ',
+          C = '<C-…> ',
+          M = '<M-…> ',
+          D = '<D-…> ',
+          S = '<S-…> ',
+          CR = '<CR> ',
+          Esc = '<Esc> ',
+          ScrollWheelDown = '<ScrollWheelDown> ',
+          ScrollWheelUp = '<ScrollWheelUp> ',
+          NL = '<NL> ',
+          BS = '<BS> ',
+          Space = '<Space> ',
+          Tab = '<Tab> ',
+          F1 = '<F1>',
+          F2 = '<F2>',
+          F3 = '<F3>',
+          F4 = '<F4>',
+          F5 = '<F5>',
+          F6 = '<F6>',
+          F7 = '<F7>',
+          F8 = '<F8>',
+          F9 = '<F9>',
+          F10 = '<F10>',
+          F11 = '<F11>',
+          F12 = '<F12>',
+        },
+      },
+    },
+  },
+
 
   'tpope/vim-sleuth',
   { -- Git signs in the gutter
@@ -412,4 +464,7 @@ return {
       indent = { enable = true, disable = { 'ruby' } },
     },
   },
+
+  -- Import all from plugins dir
+  { import = 'plugins' },
 }
