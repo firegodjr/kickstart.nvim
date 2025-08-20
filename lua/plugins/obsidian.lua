ee1c87e0cf508dec0c78f5caa5006a79a1347e82
Ethan Schrunk <ethan.schrunk@knowledgelake.com>
2025-08-20 13:56:06 -0500

    Only create workspace if found during setup

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,3 +1,21 @@
+local function getWorkspaces()
+  local workspaces = {}
+  -- Only add OneDrive workspace if path exists
+  -- Check for both Linux and Windows VMs
+  if (vim.fn.isdirectory('/media/sf_OneDrive_KnowledgeLake/Obsidian/') == 1) then
+    table.insert(workspaces, {
+      name = 'kl_onedrive',
+      path = '/media/sf_OneDrive_KnowledgeLake/Obsidian/',
+    })
+  elseif (vim.fn.isdirectory(vim.fn.expand('~/OneDrive - KnowledgeLake/Obsidian/') == 1)) then
+    table.insert(workspaces, {
+      name = 'kl_onedrive',
+      path = '~/OneDrive - KnowledgeLake/Obsidian/',
+    })
+  end
+  return workspaces;
+end
+
 return {
   'epwalsh/obsidian.nvim',
   dependencies = {
@@ -7,11 +25,8 @@ return {
   lazy = true,
   ft = 'markdown',
   opts = {
-    workspaces = {
-      {
-        name = 'kl_onedrive',
-        path = '/media/sf_OneDrive_KnowledgeLake/Obsidian/',
-      },
-    },
+    workspaces = getWorkspaces()
   },
 }
+
+
