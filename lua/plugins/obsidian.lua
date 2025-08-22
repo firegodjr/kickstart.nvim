a3564a50937cebca3760b5ac1252d1f542df27e4
Ethan Schrunk <ethan.schrunk@knowledgelake.com>
2025-08-22 11:09:39 -0500

    Fix missing obsidian workspace

--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -1,3 +1,23 @@
+-- Check first if workspace exists
+local workspaces = {}
+
+if vim.fn.isdirectory('/media/sf_OneDrive_KnowledgeLake/Obsidian/') == 1 then
+  table.insert(workspaces, {
+    {
+      name = 'kl_onedrive',
+      path = '/media/sf_OneDrive_KnowledgeLake/Obsidian/',
+    },
+  })
+end
+if vim.fn.isdirectory(vim.fn.expand('~/OneDrive - KnowledgeLake/Obsidian/')) == 1 then
+  table.insert(workspaces, {
+    {
+      name = 'kl_onedrive',
+      path = vim.fn.expand('~/OneDrive - KnowledgeLake/Obsidian/'),
+    },
+  })
+end
+
 return {
   'epwalsh/obsidian.nvim',
   dependencies = {
@@ -7,11 +27,6 @@ return {
   lazy = true,
   ft = 'markdown',
   opts = {
-    workspaces = {
-      {
-        name = 'kl_onedrive',
-        path = '/media/sf_OneDrive_KnowledgeLake/Obsidian/',
-      },
-    },
+    workspaces = workspaces,
   },
 }
